83a9552183b394e0c475c3bb817f5ef7a5a73011
Nickothan <s1nicolasrg@gmail.com>
2021-04-19 03:44:56 -0500

    Mostrando movimientos anteriores

--- src/index.js
+++ src/index.js
@@ -94,6 +94,16 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        /* Mapeando el historial */
+        const moves = history.map((step, move) => {
+            const desc = move ? "go to move #" + move : "Go to game start";
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         /* Imprimiendo resultado turno y ganador en el dom */
         if (winner) {
@@ -113,7 +123,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
