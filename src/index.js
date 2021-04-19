b4fba7e43d01c7993f6b685b67783e20a68b475e
Nickothan <s1nicolasrg@gmail.com>
2021-04-19 02:07:56 -0500

    Declarando ganador

--- src/index.js
+++ src/index.js
@@ -30,6 +30,11 @@ class Board extends React.Component {
         /* Agregamos la inmutabilidad haciendo copias con la propiedad slice() al hacer click*/
         const squares = this.state.squares.slice();
 
+        /*Ignorar cambios cuando hay un ganador o el cuadro esta lleno */
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
         /* Alternar  turnos X y O con if*/
         squares[i] = this.state.xIsNext ? "X" : "O";
 
@@ -53,8 +58,15 @@ class Board extends React.Component {
     }
 
     render() {
-        /*Cambio de  status */
-        const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            /*Declarando a un ganador*/
+            status = "Winner " + winner;
+        } else {
+            /*Cambio de  status */
+            status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        }
 
         /* Datos de rejilla que pasaremos a Square por medio de renderSquare */
         return (
@@ -99,3 +111,24 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+/* Declarando un ganador*/
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
